c178c8b6d2dd85d68fc2de22447d2ef62ca78fd9
Olanrewaju Farooq <olanrewajufarooq@yahoo.com>
2025-03-09 21:22:18 +0300

    [ADD]: Files for Simscape Modelling

---
+++ params.m
@@ -0,0 +1,57 @@
+clc; clear;
+
+%% General Specs
+mass = 3;
+n_prop = 6;
+DC_Motor_voltage=10;
+
+% Centre Cylinder
+r_ctr_cyl = 0.25;
+l_ctr_cyl = 0.05;
+v_ctr_cyl = pi * r_ctr_cyl^2 * l_ctr_cyl;
+
+% Arms
+r_arm = 0.02;
+l_arm = 0.5;
+v_arm = pi * r_arm^2 * l_arm;
+
+% Motors
+r_motor = 0.02;
+l_motor = 0.02;
+v_motor = pi * r_motor^2 * l_motor;
+
+% Propeller - Modeled as a cuboid
+    % Length: 9.4″.
+    % Pitch: 4.3″.
+    % Weight: 24 gm.
+    % Shaft Diameter: 8 mm.
+    % Total Length: 9.4 inch / 225 mm
+l_prop = 0.225; %m
+m_prop = 0.024; %kg
+w_prop = l_prop/5;
+prop_torque_cw = -0.1; %N.m
+prop_torque_ccw = -0.1;
+
+% Position of arms
+
+rot_z = @(ang) [cosd(ang) -sind(ang) 0; sind(ang), cosd(ang), 0; 0, 0, 1];
+
+x_pos_arm = zeros(1, n_prop);
+y_pos_arm = zeros(1, n_prop);
+
+arm_pos = struct();
+
+for i = 1:n_prop
+    arm_pos(i).pos = [r_ctr_cyl * cosd(60*(i-1)), r_ctr_cyl * sind(60*(i-1)), 0];
+    arm_pos(i).rot = rot_z(60*(i-1));
+end
+
+% Compute Densities
+volume = v_ctr_cyl + n_prop*v_arm; 
+
+density = mass / volume;
+
+m_ctr_cyl = density * v_ctr_cyl;
+m_arm = density * v_arm;
+
+m_motor = density * v_motor;
