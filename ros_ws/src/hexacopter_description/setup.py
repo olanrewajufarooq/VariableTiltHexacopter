9a15118b16b0518673cd752fbb48c738a14a93f9
Farooq Olanrewaju <olanrewajufarooq@yahoo.com>
2025-04-09 19:44:34 +0300

    [EDIT]: Updating setup file for yaml files and hover control node

--- ros_ws/src/hexacopter_description/setup.py
+++ ros_ws/src/hexacopter_description/setup.py
@@ -12,6 +12,7 @@ setup(
     data_files=[
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.sdf')),
         (os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf')),
         (os.path.join('share/ament_index/resource_index/packages'), ['resource/' + package_name]),
@@ -20,11 +21,13 @@ setup(
     zip_safe=True,
     maintainer='Farooq Olanrewaju',
     maintainer_email='olanrewajufarooq@yahoo.com',
-    description='Hexacopter variable tilt description and simulation launch',
+    description='Simulator for Geometric Control of Variable Tilt Hexacopter',
     license='MIT',
     entry_points={
         'console_scripts': [
             'simple_control_node = hexacopter_description.simple_control_node:main',
+            'control_allocation_node = hexacopter_description.control_allocation_node:main',
+            'hover_control_node = hexacopter_description.hover_control_node:main',
         ],
     },
 )
